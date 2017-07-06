fe038df36a9857d49252fe640d2865a20d378ee8
Martin Takáč <martin@takac.name>
2017-07-06 17:34:44 +0200

    QueryModel

---
+++ libs/Taco/Nette/Forms/interfaces.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Copyright (c) since 2004 Martin Takáč (http://martin.takac.name)
+ * @license   https://opensource.org/licenses/MIT MIT
+ */
+
+namespace Taco\Nette\Forms;
+
+
+/**
+ * @author	 Martin Takáč <martin@takac.name>
+ */
+interface QueryModel
+{
+
+	/**
+	 * @param string $term
+	 * @param numeric $page
+	 * @param numeric $pageSize
+	 * @return {total:numeric, items:array of {id:string, label:string}}
+	 */
+	function range($term, $page, $pageSize);
+
+
+	/**
+	 * @param string $id
+	 * @return {id:string, label:string}
+	 */
+	function read($id);
+
+}
