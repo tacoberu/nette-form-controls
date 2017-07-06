983b69e00957281273a1ede6a6701c9d921b2c12
Martin Takáč <martin@takac.name>
2017-07-06 16:25:40 +0200

    add: ColorInput

---
+++ libs/Taco/Nette/Forms/Controls/ColorInput.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Copyright (c) since 2004 Martin Takáč (http://martin.takac.name)
+ * @license   https://opensource.org/licenses/MIT MIT
+ */
+
+namespace Taco\Nette\Forms\Controls;
+
+use Nette\Forms\Form,
+	Nette\Forms\Controls\TextInput;
+
+
+/**
+ * @author Martin Takáč <martin@takac.name>
+ */
+class ColorInput extends TextInput
+{
+
+	const PATTERN = '/^(#[0-9a-fA-F]{6})|transparent|inherit$/';
+
+
+	/**
+	 * @param string
+	 * @throws InvalidArgumentException
+	 */
+	function __construct($label = Null)
+	{
+		parent::__construct($label);
+		$this->addCondition(Form::FILLED)
+				->addRule(Form::PATTERN, 'Color must be in hex format.', self::PATTERN);
+		$this->control->{'data-type'} = 'color';
+	}
+
+}
