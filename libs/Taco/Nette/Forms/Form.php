0fed519bac1801f5c408602caa6536d689a34768
Martin Takáč <martin@takac.name>
2019-09-07 01:54:09 +0200

    add Form with inicialization callback.

---
+++ libs/Taco/Nette/Forms/Form.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Taco\Nette\Application\UI;
+
+use Nette;
+
+
+/**
+ * Vylepšení:
+ * - vyžaduje callback pro získání inicializačních dat.
+ */
+class Form extends Nette\Application\UI\Form
+{
+
+	/**
+	 * @var callback
+	 */
+	private $initialize;
+
+	/**
+	 * @var string
+	 */
+	private $name;
+
+	function __construct($initialize, $parent = null, $name = null)
+	{
+		parent::__construct($parent, $name);
+		$this->name = $name;
+		$this->initialize = $initialize;
+		$this->monitor('Nette\Application\IPresenter');
+	}
+
+
+
+	function getFlowName()
+	{
+		return $this->name;
+	}
+
+
+
+	protected function attached($presenter)
+	{
+		parent::attached($presenter);
+		if ($presenter instanceof Nette\Application\IPresenter) {
+			if ( ! $this->isAnchored() || ! $this->isSubmitted()) {
+				if ($cb = $this->initialize) {
+					$this->setDefaults((array) $cb());
+				}
+			}
+		}
+	}
+
+}
